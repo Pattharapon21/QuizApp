92e6a202ffc15f88a20e1c553547493748f686b0
Pattharapon21 <phattharaphon251046@gmail.com>
2025-02-05 11:12:12 +0700

    Add files via upload

---
+++ questions_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/answer_button.dart';
+import 'package:myapp/data/quizz.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/result_screen.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  int currentQuestionIndex = 0;
+
+  //Create array to store the selected Answers
+  final List<String> selectedAnswers = [];
+
+  void answerQuestion(String selectedAnswer) {
+    setState(() {
+      selectedAnswers.add(selectedAnswer);
+      if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+      } else {
+        // Navigate to the Result Screen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => 
+            ResultScreen(selectedAnswers: selectedAnswers),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.purple],
+            ),
+          ),
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.all(40),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    currentQuestion.question,
+                    style: GoogleFonts.lato(
+                        color: const Color.fromARGB(255, 6, 80, 184),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ...currentQuestion.getShuffledAnswers().map(
+                    (answer) {
+                      return AnswerButton(
+                          answer: answer, onTap: () => answerQuestion(answer));
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
